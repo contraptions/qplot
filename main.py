95763ec093566de4865182be7c80e857366c8eef
jdh <joe.hickie@hotmail.co.uk>
2022-03-18 11:06:52 +0000

    things

--- main.py
+++ main.py
@@ -9,5 +9,21 @@ from qplot import PlotterWrite
 folder = './example_data_dir'
 
 """
-qplot.live_plot(data_dir=folder)
+plot = qplot.2d_live_plot(folder=folder) 
+
+plot.axes(
+    x_data=np.linspace(0, 10, 100), 
+    y_data=np.linspace(0, 10, 100)
+)
+
+while True: 
+    plot.data(np.random.rand(100, 100))
+    
+"""
+
+"""
+Want the syntax to look something like above. If y_data is not given then the plot can be assumed to be
+1d and then plot.data() plots a 1d plot. 
+
+Get rid of the variables - this is useful for qgor but maybe not useful here? 
 """
--- qplot/qplot.py
+++ qplot/qplot.py
@@ -7,11 +7,36 @@ Created on 11/03/2022
 The main class for qplot. 
 """
 
-class QPlot:
+from .PlotWindows import PlotterWrite
+
+class TwoDLive:
 
     def __init__(self, folder):
-
         self.folder = folder
+
+    def axes(self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
+
+        self.x_
+
+    def make_plotter(self):
+
+        variables=[https://github.com/contraptions/qplot.git
+
+        ]
+
+        self.plotter = PlotterWrite(
+            folder=self.folder,
+            self.variables=
+        )
+
+
+
+
+
+
+
 
 
 
